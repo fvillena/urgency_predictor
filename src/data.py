6615e743440d8b53a1d458afa7037312b8453a11
Fabián Villena <villenafabian@gmail.com>
2020-03-06 09:25:54 -0300

    omitting last row

--- src/data.py
+++ src/data.py
@@ -4,7 +4,7 @@ class Transformer:
     def __init__(self,raw_data_file_path):
         self.raw_data_file_path = raw_data_file_path
         self.wd = "/".join(raw_data_file_path.split("/")[:-1])+"/"
-        self.df = pd.read_excel(raw_data_file_path,skiprows=16,skipfooter=560,na_values="-")
+        self.df = pd.read_excel(raw_data_file_path,skiprows=16,skipfooter=561,na_values="-")
         self.df = self.df.melt(id_vars=['Total de atenciones de urgencia'])
         self.df.columns = ["category","date","value"]
         self.df.date = pd.to_datetime(self.df.date)
