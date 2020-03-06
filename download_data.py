d744b1ee53acc18b3ca9110e58714f4b9fe1de64
Fabián Villena <villenafabian@gmail.com>
2020-03-06 12:18:17 -0300

    db connection

--- download_data.py
+++ download_data.py
@@ -1,8 +1,12 @@
 from src.scraper import Scraper
 from src.data import Transformer
 import os
+from src.data import DatabaseWriter
 wd = os.getcwd() + "/"
 escreiper = Scraper(wd)
 scraped_data_file_path = escreiper.scrape()
 transformer = Transformer(scraped_data_file_path)
 transformer.write()
+
+dw = DatabaseWriter(wd+"/../urgency_predictor_data/data.sqlite","real","/home/fvillena/code/urgency_predictor/real_data.csv")
+dw.write()
