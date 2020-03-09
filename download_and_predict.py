1a7264b6387be9ab287e63ecbe4a19d009070f54
Fabián Villena <villenafabian@gmail.com>
2020-03-09 11:26:37 -0300

    download and predict

--- download_and_predict.py
+++ download_and_predict.py
@@ -2,6 +2,7 @@ from src.scraper import Scraper
 from src.data import Transformer
 import os
 from src.data import DatabaseWriter
+from src.predictor import Predictor
 wd = os.getcwd() + "/"
 escreiper = Scraper(wd)
 scraped_data_file_path = escreiper.scrape()
@@ -10,3 +11,9 @@ transformer.write()
 
 dw = DatabaseWriter(wd+"../urgency_predictor_data/data.sqlite","real",wd+"real_data.csv")
 dw.write()
+
+wd = os.getcwd() + "/"
+p = Predictor(wd+"../urgency_predictor_data/data.sqlite")
+p.fit()
+p.predict(60)
+p.write()
--- predict_data.py
+++
@@ -1,8 +0,0 @@
-from src.predictor import Predictor
-import os
-wd = os.getcwd() + "/"
-p = Predictor(wd+"../urgency_predictor_data/data.sqlite")
-p.fit()
-p.predict(60)
-p.write()
-print("foo")
